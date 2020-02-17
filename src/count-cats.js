c491e2c7024772d57e1c9dde9cea51353e8d751a
vladislav t <vt118@mail.ru>
2020-02-18 00:15:18 +0300

    feat: make countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let result = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr[i].length; j++) {
+    if(arr[i][j] == '^^') {
+      result += 1;
+    }
+  }
+}
+  
+  return result;
 };
