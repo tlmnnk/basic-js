12fdf52c0fb936d3ffd25182ee0dbc83cb31a0ab
vladislav t <vt118@mail.ru>
2020-02-18 19:33:23 +0300

    feat: add transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let result = [];
+    arr.forEach((item, i ) => {
+     
+        switch (item) {
+            case '--discard-next':
+
+                break;
+            case '--discard-prev':
+                result.length !== 0 ? result.pop() : null;
+                break;
+            case '--double-next':
+                i < arr.length - 1 ? result.push(arr[i + 1]) : null;
+                break;
+            case '--double-prev':
+                i >= 1 ? result.push(arr[i - 1]) : null;
+                break;
+            default:
+                 result.push(arr[i]);
+                    if(arr[i - 1] === '--discard-next') {
+                        result.pop();
+                    }
+                }
+
+    });
+    return result;
 };
