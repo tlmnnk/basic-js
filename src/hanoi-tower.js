796d4e4c493c9d1296565a616cd1545f960f8766
vladislav t <vt118@mail.ru>
2020-02-19 17:38:09 +0300

    feat: add hanoi-tower function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,10 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+
+    const turns = Math.pow(2, disksNumber) - 1;
+    const speed = turns / (turnSpeed / 3600);
+
+    return {
+        turns: turns,
+        speed: speed
+    };
+};
