d9d7e1cd017c73b4f7dd5b273b60a13f982f88a0
vladislav t <vt118@mail.ru>
2020-02-25 20:26:53 +0300

    fix: what season correction

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,15 @@ module.exports = function getSeason(date) {
   if(arguments.length === 0) {
     return 'Unable to determine the time of year!';
   }
-  if(typeof date != 'object') {
+  if(Object.keys(date).length > 0) {
     throw new Error('Bang!');
   }
 
-  switch(date.getMonth()) {
-    case 0 || 1 || 11:
-      return 'Winter';
-    case 2 || 3 || 4:
-      return 'Spring';
-    case 5 || 6 || 7:
-      return 'Summer';
-    case 8 || 9 || 10:
-        return 'Fall';
-  }
+  const month = date.getMonth() + 1;
 
+    if(month === 12 || month < 3) return 'winter';
+    if(month > 2 && month < 6) return 'spring';
+    if(month > 5 && month < 9) return 'summer';
+    if(month > 8 && month < 12) return 'fall';
   
 };
