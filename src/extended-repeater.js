f84224f71d6685809436c55848e0bfde08f2f9d5
vladislav t <vt118@mail.ru>
2020-02-25 19:57:04 +0300

    feat: implement extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,49 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+
+    if(typeof str !== 'string') {
+        str = str + '';
+      }
+
+    let result = '';
+
+    if(!options.repeatTimes) options.repeatTimes = 1;
+
+    if(!options.separator) options.separator = '+';
+
+    if(options.hasOwnProperty('addition')) {
+        if(typeof options.addition !== 'string') {
+            options.addition = options.addition + '';
+          }
+        if(!options.additionRepeatTimes)
+          options.additionRepeatTimes = 1;
+  
+        if(!options.additionSeparator)
+          options.additionSeparator = '|';
+    } else options.addition = '';
+
+    if(!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+
+    if(!options.additionSeparator) options.additionSeparator = '|';
+
+    
+
+    if(!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+    let i = 0;
+    while(i < options.repeatTimes) {
+        let j = 0;
+        result += str;
+        if(options.addition != '') {
+            while(j < options.additionRepeatTimes) {
+                result += options.addition;
+                if( j + 1 !== options.additionRepeatTimes )
+                result += options.additionSeparator;
+                j++;
+            }
+        }
+    if( i + 1 !== options.repeatTimes )
+    result += options.separator;
+    i++;
+    }
+    return result;
 };
   
